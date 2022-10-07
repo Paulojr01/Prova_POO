7a9c7a9478e39b210dcf6a873419c5ee3c9ffda5
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2022-10-06 22:09:16 -0300

    ok

---
+++ br.com.ex01.prova/TrabalhoVoluntario.java
@@ -0,0 +1,12 @@
+public class TrabalhoVoluntario extends Projeto {
+    private String tipoTrabalho;
+    private int duracaoTrabalho;
+
+    public TrabalhoVoluntario(String nomeProjeto, String descricao, String endereco, String datainicio,
+            String datafim) {
+        super(nomeProjeto, descricao, endereco, datainicio, datafim);
+        //TODO Auto-generated constructor stub
+    }
+
+    
+}
